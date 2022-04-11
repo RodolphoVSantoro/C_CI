b02fb35c11fdcdc73e5d2830abde6d8156140a1d
RodolphoVSantoro <44331003+RodolphoVSantoro@users.noreply.github.com>
2022-04-11 18:47:19 -0300

    Update test_1.c

--- test/test_1.c
+++ test/test_1.c
@@ -1,1 +1,36 @@
 #include "../src/lib.h"
+#include <cppunit/TestRunner.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+
+class SomaTest : public CPPUNIT_NS::TestCase
+{
+  SomaTest( std::string name ) : CppUnit::TestCase( name ) {}
+  
+  void runTest() {
+    CPPUNIT_ASSERT( soma(10, 1) == 11 );
+};
+
+
+CPPUNIT_TEST_SUITE_REGISTRATION(SomaTest);
+
+int main()
+
+{
+  CPPUNIT_NS::TestResult controller;
+
+  CPPUNIT_NS::TestResultCollector result;
+  controller.addListener(&result);
+
+  CPPUNIT_NS::BriefTestProgressListener progress;
+  controller.addListener(&progress);
+
+  CPPUNIT_NS::TestRunner runner;
+  runner.addTest(CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest());
+  runner.run(controller);
+
+  return result.wasSuccessful() ? 0 : 1;
+}
