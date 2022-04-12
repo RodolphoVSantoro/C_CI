bae78e45939a131b945e0a2ccebbf30f0468a782
RodolphoVSantoro <44331003+RodolphoVSantoro@users.noreply.github.com>
2022-04-12 09:57:45 -0300

    Update test_1.cpp

--- test/test_1.cpp
+++ test/test_1.cpp
@@ -10,12 +10,18 @@ class SomaTest : public CPPUNIT_NS::TestCase
 {
   SomaTest( std::string name ) : CppUnit::TestCase( name ) {}
   
-  void runTest() {
+  void testeSoma() {
     CPPUNIT_ASSERT( soma(10, 2) == 12 );
   }
 };
 
+  CPPUNIT_TEST_SUITE(SomaTest);
+  CPPUNIT_TEST(testeSoma);
+  CPPUNIT_TEST_SUITE_END();
+  CPPUNIT_TEST_SUITE_REGISTRATION( MyTest );
+
 int main(){
+
   CPPUNIT_NS::TestResult controller;
 
   CPPUNIT_NS::TestResultCollector result;
